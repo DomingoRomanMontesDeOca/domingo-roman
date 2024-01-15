5a289f1f43dc49c1999f09b688de357bff9352cd
Skyrdow <lucas.mesias@usach.cl>
2024-01-15 19:11:00 -0300

    fix tagline

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,12 +20,6 @@ export default defineConfig({
     sidebar: [{
       label: 'Proyectos de investigación',
       link: '/proyectos'
-      // items: [
-      // // Each item here is one entry in the navigation menu.
-      // {
-      //   label: 'Example Guide',
-      //   link: '/guides/example/'
-      // }]
     }, {
       label: 'Publicaciones',
       link: '/publicaciones'
