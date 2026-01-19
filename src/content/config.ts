086a41708f3778d872d27653faa2507c0c4511e7
Domingo Roman Montes de Oca <domingo.roman@usach.cl>
2026-01-18 21:55:57 -0300

    Update config.ts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,6 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 import { docsSchema } from '@astrojs/starlight/schema';
 
+// Extender el esquema de Starlight
+const starlightSchema = docsSchema().extend({
+  sidebar: z.object({
+    label: z.string(),
+    order: z.number(),
+  }).optional(),
+});
+
 export const collections = {
-	docs: defineCollection({ schema: docsSchema() }),
+  docs: defineCollection({ 
+    schema: starlightSchema, 
+  }),
 };
